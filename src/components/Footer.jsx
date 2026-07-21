b61281a76a73a04bac00f637864b82d64f5057fc
busraydinn <123876140+busraydinn@users.noreply.github.com>
2026-07-21 23:52:50 +0300

    fix router paths card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 function Footer() {
+
   return (
-    <div className='h-15 bg-slate-100 flex flex-col justify-around items-center text-slate-400 text-[14px]'>
-      <h1>© 2026 TravelApp All right reserved.</h1>
-
-      <div className='flex justify-between w-50'>
-        <Link to="/" className='hover:text-blue-500'>
-          Anasayfa
-        </Link>
-
-        <Link to="/contact" className='hover:text-blue-500'>
-          İletişim
-        </Link>
-
-        <Link to="/about" className='hover:text-blue-500'>
-          Hakkımızda
-        </Link>
-      </div>
+    <div className='h-15 bg-slate-100 flex flex-col justify-around items-center text text-slate-400 text-[14px]'>
+            <h1> © 2026 TravelApp All right reserved.</h1>
+            <div className='flex justify-between w-50 '>
+                <Link to="/travelapp" className='hover:text-blue-500'>Anasayfa</Link>
+                <Link to="/travelapp/contact" className='hover:text-blue-500'>İletişim</Link>
+                <Link to="/travelapp/about" className='hover:text-blue-500'>Hakkımızda</Link>
+            </div>
     </div>
   )
 }
 
 export default Footer;
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="sticky top-0 z-50 border-b border-gray-200 bg-white shadow-sm">
       <div className="container mx-auto flex h-16 items-center justify-between px-6">
         <Link
-          to="/"
+          to="/travelapp"
           className="text-2xl font-extrabold tracking-wide text-slate-700 transition hover:text-slate-900"
         >
           Traveler
